4de2b22b915f8a8efe49d231e349f2abebca77e9
Luis Pomare <luispomare92@gmail.com>
2026-03-02 18:08:38 -0500

    Add fromWithSubmit functionality

--- src/components/FormWithSubmit.jsx
+++ src/components/FormWithSubmit.jsx
@@ -1,5 +1,72 @@
+import { useState } from 'react';
+
 function FormWithSubmit() {
-  return <p>This is going to be a form with submit</p>;
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+    currentEmployee: '',
+  });
+
+  function onChangeHandler(event) {
+    const { name, value } = event.target;
+
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  function onSubmitHandler(event) {
+    event.preventDefault();
+
+    console.log(formState.email, formState.password, formState.currentEmployee);
+
+    setFormState({
+      email: '',
+      password: '',
+      currentEmployee: '',
+    });
+  }
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <input
+        type='email'
+        value={formState.email}
+        placeholder='Enter your email'
+        onChange={onChangeHandler}
+        name='email'
+      />
+
+      <input
+        type='password'
+        value={formState.password}
+        placeholder='Enter your password'
+        onChange={onChangeHandler}
+        name='password'
+      />
+
+      <input
+        type='radio'
+        name='currentEmployee'
+        value='yes'
+        checked={formState.currentEmployee === 'yes'}
+        onChange={onChangeHandler}
+      />
+
+      <input
+        type='radio'
+        name='currentEmployee'
+        value='no'
+        checked={formState.currentEmployee === 'no'}
+        onChange={onChangeHandler}
+      />
+
+      <button type='submit' disabled={!formState.email || !formState.password}>
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default FormWithSubmit;
